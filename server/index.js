05179002ab640df2a395246abfec221b9b344ef3
Nishan9t <pandeynisha9t@gmail.com>
2023-07-31 17:49:47 +0530

    database connected successfully

--- server/index.js
+++ server/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
-
 const app= express();
+const cors=require('cors');
+const mongoose= require('mongoose');
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/jwt",{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("DB connection successsful");
+}).catch(err =>{
+    console.log(err.message);
+})
 
 app.listen(8000,()=>{
     console.log("Server started on port 8000");
 })
+
