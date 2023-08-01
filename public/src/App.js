8a38cb7a16938123462df522cf667bf09659e8b6
Nishan9t <pandeynisha9t@gmail.com>
2023-08-01 16:07:18 +0530

    routes are created and css structure is made

--- public/src/App.js
+++ public/src/App.js
@@ -1,8 +1,18 @@
-
+import {BrowserRouter , Routes, Route} from 'react-router-dom';
 import React from 'react'
+import Register from './pages/Register';
+import Secret from './pages/Secret';
+import Login from './pages/Login';
 
 export default function App() {
   return (
-    <div>App</div>
+  <BrowserRouter>
+    <Routes>
+      <Route exact path="/register" element={<Register/>} />
+      <Route exact path="/login" element={<Login/>} />
+      <Route exact path="/" element={<Secret/>} />
+    </Routes>
+    
+  </BrowserRouter>
   )
 }
